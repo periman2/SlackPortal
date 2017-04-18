88f683a0867cb2be7636eb11035fed0408908a6c
periman22 <periman@windowslive.com>
2017-04-18 21:53:53 +0300

    Testing regex agaaaain

--- public/ajax.js
+++ public/ajax.js
@@ -118,9 +118,8 @@ $(document).ready(function(){
                 "<div class='avattext'><img src=" + avatar + " alt='avatar' class='avatar'>" + "<div class='flexnone'><h3>" + sender + "</h3><p>" + text + "</p></div></div>");
         });
         $(".text").scrollTop($(".text").get(0).scrollHeight);
-        emojify.run($(".text")[0]);
     }
-    emojify.setConfig();
+    emojify.run();
     
 
 });
